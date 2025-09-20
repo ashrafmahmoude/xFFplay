8f37b264ac0fd9e41b031a9c00fa12097e349cac
ashrafmahmoude <121548623+ashrafmahmoude@users.noreply.github.com>
2025-09-20 17:35:22 +0300

    Initial commit

---
+++ xFFplay/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using Microsoft.Win32;
+using System.Windows.Forms.VisualStyles;
+
+namespace xFFplay
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        Form2 form2 = new Form2();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.EnableVisualStyles();
+            this.DoubleBuffered = true;
+
+            
+        }
+        public Process ffplay = new Process();
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            form2.Show();
+            
+
+            xxxFFplay();
+        }
+        private void xxxFFplay()
+        {
+            
+             
+           
+            ffplay.StartInfo.FileName = "c:\\ffmpeg\\ffplay.exe";
+            ffplay.StartInfo.Arguments = "-noborder c:\\ffmpeg\\bay.mp4";
+            
+            ffplay.StartInfo.CreateNoWindow = true;
+            ffplay.StartInfo.RedirectStandardOutput = true;
+            ffplay.StartInfo.UseShellExecute = false;
+            
+
+            ffplay.EnableRaisingEvents = true;
+            ffplay.OutputDataReceived += (o, e) => Debug.WriteLine(e.Data ?? "NULL", "ffplay");
+            ffplay.ErrorDataReceived += (o, e) => Debug.WriteLine(e.Data ?? "NULL", "ffplay");
+            ffplay.Exited += (o, e) => Debug.WriteLine("Exited", "ffplay");
+            ffplay.Start();
+
+           // Thread.Sleep(5000); // you need to wait/check the process started, then...
+           timer1.Enabled = true;
+            // child, new parent
+            // make 'this' the parent of ffmpeg (presuming you are in scope of a Form or Control)
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { ffplay.Kill(); }
+            catch { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            SetParent(ffplay.MainWindowHandle, form2.Handle);
+
+            // window, x, y, width, height, repaint
+            // move the ffplayer window to the top-left corner and set the size to 320x280
+            MoveWindow(ffplay.MainWindowHandle, 0, 0, form2.Width, form2.Height, true);
+            timer1.Enabled=false;
+        }
+    }
+}
---
+++ xFFplay/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xFFplay")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xFFplay")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a589411-89ba-46af-be18-7e3344107240")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
